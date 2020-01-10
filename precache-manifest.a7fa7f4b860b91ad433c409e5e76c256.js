b26de8c32925370df90fa5f0775f6d7c5378c9ba
Dixshant Gupta <dixshantgupta@gmail.com>
2020-01-10 15:13:16 +0530

    Updates

---
+++ precache-manifest.a7fa7f4b860b91ad433c409e5e76c256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e124740e33b128bb94e930761b2a1a5f",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "4ee659a6f53bed7485a5",
+    "url": "/monsters/static/css/main.f4b5d371.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monsters/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ee659a6f53bed7485a5",
+    "url": "/monsters/static/js/main.b8099b7d.chunk.js"
+  },
+  {
+    "revision": "89da4785623cb713b6bb",
+    "url": "/monsters/static/js/runtime-main.3ed6cfb5.js"
+  }
+]);
